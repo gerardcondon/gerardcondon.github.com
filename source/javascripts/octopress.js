d1309bb5672a3e26561414137c178bfa6b38c896
Gerard Condon <git@gerardcondon.com>
2016-11-21 23:02:47 +0000

    Fixing the javascript after including JQuery

--- source/javascripts/octopress.js
+++ source/javascripts/octopress.js
@@ -2,15 +2,16 @@ function getNav() {
   var mobileNav = $('nav[role=navigation] fieldset[role=search]').after('<fieldset class="mobile-nav"></fieldset>').next().append('<select></select>');
   mobileNav.children('select').append('<option value="">Navigate&hellip;</option>');
   $('ul[role=main-navigation]').addClass('main-navigation');
-  $('ul.main-navigation a').each(function(link) {
+  $('ul.main-navigation a').each(function(i, link) {
     mobileNav.children('select').append('<option value="'+link.href+'">&raquo; '+link.text+'</option>');
   });
-  $('ul.subscription a').each(function(link) {
+  $('ul.subscription a').each(function(i, link) {
     mobileNav.children('select').append('<option value="'+link.href+'">&raquo; '+link.text+'</option>');
   });
   mobileNav.children('select').bind('change', function(event) {
     if (event.target.value) { window.location.href = event.target.value; }
   });
+  mobileNav.children('select').val('');
 }
 
 function addSidebarToggler() {
@@ -27,7 +28,7 @@ function addSidebarToggler() {
   }
   var sections = $('aside.sidebar > section');
   if (sections.length > 1) {
-    sections.each(function(section, index){
+    sections.each(function(index, section){
       if ((sections.length >= 3) && index % 3 === 0) {
         $(section).addClass("first");
       }
@@ -39,13 +40,11 @@ function addSidebarToggler() {
 }
 
 function testFeatures() {
-  var features = ['maskImage'];
-  $(features).map(function(feature) {
-    if (Modernizr.testAllProps(feature)) {
-      $('html').addClass(feature);
-    } else {
-      $('html').addClass('no-'+feature);
-    }
+  var features = ['maskImage'];   
+  $(features).map(function(i,feature){     
+    Modernizr.addTest(feature,function(){
+      Modernizr.testAllProps(feature)
+    });
   });
   if ("placeholder" in document.createElement("input")) {
     $('html').addClass('placeholder');
@@ -56,7 +55,7 @@ function testFeatures() {
 
 function addCodeLineNumbers() {
   if (navigator.appName === 'Microsoft Internet Explorer') { return; }
-  $('div.gist-highlight').each(function(code) {
+  $('div.gist-highlight').each(function(i, code) {
     var tableStart = '<table><tbody><tr><td class="gutter">',
         lineNumbers = '<pre class="line-numbers">',
         tableMiddle = '</pre></td><td class="code">',
@@ -73,7 +72,7 @@ function addCodeLineNumbers() {
 function flashVideoFallback(){
   var flashplayerlocation = "/assets/jwplayer/player.swf",
       flashplayerskin = "/assets/jwplayer/glow/glow.xml";
-  $('video').each(function(video){
+  $('video').each(function(i, video){
     video = $(video);
     if (!Modernizr.video.h264 && swfobject.getFlashPlayerVersion() || window.location.hash.indexOf("flash-test") !== -1){
       video.children('source[src$=mp4]').first().map(function(source){
@@ -93,14 +92,14 @@ function flashVideoFallback(){
 }
 
 function wrapFlashVideos() {
-  $('object').each(function(object) {
+  $('object').each(function(i, object) {
     object = $(object);
     if ( $('param[name=movie]', object).length ) {
       var wrapper = object.before('<div class="flash-video"><div>').previous();
       $(wrapper).children().append(object);
     }
   });
-  $('iframe[src*=vimeo],iframe[src*=youtube]').each(function(iframe) {
+  $('iframe[src*=vimeo],iframe[src*=youtube]').each(function(i, iframe) {
     iframe = $(iframe);
     var wrapper = iframe.before('<div class="flash-video"><div>').previous();
     $(wrapper).children().append(iframe);
